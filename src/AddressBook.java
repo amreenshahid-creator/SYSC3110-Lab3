878d2f489534e30700cca571f249aac89982a80a
amreenshahid <amreennshahidd@gmail.com>
2025-09-14 00:44:05 -0400

    Main method added that prints "Address Book"

---
+++ src/AddressBook.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList<BuddyInfo> buddyList;
+
+    public AddressBook() {
+        buddyList = new ArrayList<BuddyInfo>();
+    }
+
+    public void addBuddy(BuddyInfo buddy){
+        buddyList.add(buddy);
+    }
+
+    public void remove(BuddyInfo buddy) {
+        buddyList.remove(buddy);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+
+
+}
